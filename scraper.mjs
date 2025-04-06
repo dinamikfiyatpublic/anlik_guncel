cdf00a53a73add1675f3d2b198090761d6e33aec
denemeunv <uyal1@yandex.com>
2025-04-06 15:37:05 +0300

    Remove executablePath and update launch settings for Render

--- scraper.mjs
+++ scraper.mjs
@@ -5,11 +5,10 @@ puppeteer.use(StealthPlugin());
 
 async function scrapeData() {
   const browser = await puppeteer.launch({
-    headless: true,
-    executablePath: '/usr/bin/chromium-browser',  // Render için doğru yol
-    args: ['--no-sandbox', '--disable-setuid-sandbox']  // Güvenlik için ek argümanlar
+    headless: true, // Tarayıcıyı başlatırken arayüzsüz (headless) olarak başlatır
+    args: ['--no-sandbox', '--disable-setuid-sandbox']  // Güvenlik nedeniyle argümanlar
   });
-  
+
   const page = await browser.newPage();
   await page.goto('https://www.akakce.com/j/gl/?t=pr&i=321437397&s=0&b=315');
   await page.waitForSelector('body > ul > li');
