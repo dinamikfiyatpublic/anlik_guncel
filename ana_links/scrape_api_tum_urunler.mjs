13764dae06852f965c8f8e48ea14225c2444781d
dinamikfiyatpublic <unver.y@servify.com>
2025-05-05 14:28:50 +0000

    Add new feature and fix bugs

--- ana_links/scrape_api_tum_urunler.mjs
+++ ana_links/scrape_api_tum_urunler.mjs
@@ -120,7 +120,10 @@ async function scrapePage(page, timestamp) {
 
 
 (async () => {
-    const browser = await puppeteer.launch({ headless: true });
+    const browser = await puppeteer.launch({
+        headless: true,
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+      });
     const page = await browser.newPage();
 
     const timestamp = await getTimestamp();
